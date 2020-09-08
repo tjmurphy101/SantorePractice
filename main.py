9a5ff1b40a2c15a681612cdfb3efc94659659d83
Tjmurphy101 <t8murphy@student.bridgew.edu>
2020-09-08 11:47:39 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Welcome to")
+print("Comp151")
+
+Age = 5
+#this is a number (It's a comment)
+dog_years = 7 * Age
+print(F"The dog is {dog_years}")
+fake_years = "123" #This is not a number
+print(F"fake years is {fake_years}")
+fake_years_2 = fake_years * 7
+print(F"fake years 2 is {fake_years_2}")
